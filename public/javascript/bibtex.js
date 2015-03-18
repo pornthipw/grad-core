7c43e258b1af72a39752ffc5b4416ecffedab70c
pornthipw <pornthip.wong@gmail.com>
2015-03-18 10:00:36 +0700

    update

--- public/javascript/bibtex.js
+++ public/javascript/bibtex.js
@@ -21,6 +21,23 @@ app.filter('hide', function() {
   }
 });
 
+app.filter('orderObjectBy', function(){
+ return function(input, attribute) {
+    if (!angular.isObject(input)) return input;
+
+    var array = [];
+    for(var objectKey in input) {
+        array.push(input[objectKey]);
+    }
+
+    array.sort(function(a, b){
+        a = parseInt(a[attribute]);
+        b = parseInt(b[attribute]);
+        return a - b;
+    });
+    return array;
+ }
+});
 
 app.config(function($routeProvider) {
   $routeProvider.when('/bibtex', {
